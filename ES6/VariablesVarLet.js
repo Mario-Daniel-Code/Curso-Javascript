3b8ed3c4ad36070f5b37932671ca833c1fb20069
Mario Martinez <mario.martinez1@gmail.com>
2022-01-31 23:41:56 -0600

    Declaraciones de variables var y let

---
+++ ES6/VariablesVarLet.js
@@ -0,0 +1,27 @@
+/*-------------------------------Declaraciones de variables var vs let ------------------------------------*/
+/* Como saber en que scope estas trabajando
+* Si no estamos trabajando dentro de una funcion estamos en scope global
+* Si no estamos en una funcion el scope de una funcion
+
+GlobalScope he declarado la variable, la cual se encuentra en mi obj window
+JS lo va leyendo linea x linea hacia abajo
+EC6 tiene un nuevo tipo de inicializacion de variables
+Interprete lee var lo puede leer como un undefined
+En caso del let lo obliga js antes de ser usada, la variable debe de existir
+previamnete
+
+let mensaje = "Hola Mundo";
+console.log(mensaje);
+*/
+
+// Ejemplo
+let mensaje = "12";
+
+if(2===4){
+      mensaje = "Hola  mundo";
+}
+console.log(mensaje);
+
+
+
+
