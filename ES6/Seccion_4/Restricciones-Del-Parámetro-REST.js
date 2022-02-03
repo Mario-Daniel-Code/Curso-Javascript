45b5aa6a2cfedaf8a7ce45b022afba615e19b8d9
Mario Martinez <mario.martinez1@gmail.com>
2022-02-02 22:53:47 -0600

    Restricciones del parámetro-REST

---
+++ ES6/Seccion_4/Restricciones-Del-Parámetro-REST.js
@@ -0,0 +1,6 @@
+/*-----------------------Restricciones del parámetro-REST----------------------- */
+
+function juntar_Edades(apellidos, ...edades){
+    /*Rest parameter must be last formal parameter = > Rest parámetro debe ser el último parámetro formal */
+}
+
