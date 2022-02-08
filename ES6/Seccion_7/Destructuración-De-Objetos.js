8c78343d809284679062d6412c29f2d228fa82b1
Mario Martinez <mario.martinez1@gmail.com>
2022-02-08 17:35:26 -0600

    Destructuración de objetos

---
+++ ES6/Seccion_7/Destructuración-De-Objetos.js
@@ -0,0 +1,23 @@
+/*-----------------Destructuración de objetos----------------------*/
+
+/*Mi objeto con muchas propiedades*/
+let ajustes = {
+  nombre: "Mario Martinez",
+  email: "mario.martinez@gmail.com",
+  facebook: "mario.martinez1",
+  google: "mario.mtz12",
+  premium: true,
+};
+
+/*let nombre = ajustes.nombre;
+email = ajustes.email;
+facebook = ajustes.facebook;*/
+
+/*Aqui viene lo bueno de ES6 Destructuración*/
+/*Puedo extraer cada propiedad de mi objeto ajustes*/
+/*Si necesito cambiar de nombre alguna variable lo que puedo hacer es premium: dePago */
+/*Tambien existe la inicializacion de variables aunque no existan */
+/*No importa el orden que vayan colocadas las propiedades del objeto */
+let {premium: dePago, instagram: cuentaInsta = "Mario_00" } = ajustes;
+
+console.log(cuentaInsta, dePago);
