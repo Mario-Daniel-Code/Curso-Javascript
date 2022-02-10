dad08002398363da9b6f105e7300b08801fb2995
Mario Martinez <mario.martinez1@gmail.com>
2022-02-10 17:11:36 -0600

    Introducción - Set

---
+++ ES6/Seccion_9/Introducción-Set.js
@@ -0,0 +1,6 @@
+/*-----------------Introducción - Set---------------*/
+
+/*Sets*/
+/*Son una lista ordenada de valores sin duplicados*/
+/*Permiten un rapido acceso a la data que contienen*/
+/*Los arrays pueden tener datos duplicados*/
