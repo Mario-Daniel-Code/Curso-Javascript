68f0426ee3b174aab44289980effb67ec2a0d5fb
Mario Martinez <mario.martinez1@gmail.com>
2022-02-09 18:08:12 -0600

    Introducción a los Símbolos

---
+++ ES6/Seccion_8/Introducción-A-Los-Símbolos.js
@@ -0,0 +1,21 @@
+/*--------------------Introducción a los Símbolos--------------------*/
+
+/*Un tipo de daro unico y diferente js*/
+
+/*
+undefined
+null
+boolean
+number
+string
+object
+*/
+/*ES6 nuevo estandar
+DOM
+Es una interfaz de programación para los documentos HTML y XML. 
+Facilita una representación estructurada del documento y define de qué manera los
+programas pueden acceder, al fin de modificar, tanto su estructura, estilo y contenido.
+El DOM da una representación del documento como un grupo de nodos y objetos estructurados
+que tienen propiedades y métodos. Esencialmente, conecta las páginas web a scripts o lenguajes
+de programación.
+*/
