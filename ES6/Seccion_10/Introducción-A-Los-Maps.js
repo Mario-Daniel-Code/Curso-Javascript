ecd55dc223bd911215582eea2939161a6f26c1f6
Mario Martinez <mario.martinez1@gmail.com>
2022-02-11 13:49:44 -0600

    Introducción a los Maps

---
+++ ES6/Seccion_10/Introducción-A-Los-Maps.js
@@ -0,0 +1,14 @@
+/*----------------------Introducción a los Maps---------------------------*/
+
+
+/*Maps nuevo tipo de coleccion de datos que por mucho tiempo fue necesario en javascript*/
+
+/*Coleccion de datos que tiene una llave y un valor esto se le conoce en ingles key value pair*/
+
+/*StrongSet los maps tienen
+- Has()
+- Delete()
+- Clear()
+- Size()
+- Iteraciones()
+*/
