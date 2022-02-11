d27cc5efc8520db3ec6628dbb3750c05eff1f83a
Mario Martinez <mario.martinez1@gmail.com>
2022-02-11 14:55:17 -0600

    Inicializaciones de los mapas

---
+++ ES6/Seccion_10/Inicializaciones-De-Los-Mapas.js
@@ -0,0 +1,11 @@
+/*---------------------Inicializaciones de los mapas--------------------*/
+
+
+/*Como inicializar un mapa y que tenga valores por defecto*/
+/*Necesito recibir un parametro como arreglo y en ese arreglo recibir la llave y el valor en otros 2 arreglos */
+let mapa = new Map([["nombre", "Mario"], [undefined]]);
+
+console.log(mapa);
+console.log(mapa.get(undefined));
+
+
