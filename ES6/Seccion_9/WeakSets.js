2627efd63c69fd5647a0bdd3cbf90443f992b7b2
Mario Martinez <mario.martinez1@gmail.com>
2022-02-11 00:40:40 -0600

    WeakSets

---
+++ ES6/Seccion_9/WeakSets.js
@@ -0,0 +1,13 @@
+/*-----------------WeakSets---------------*/
+
+/*Trabajnan de una manera similara los sets, tiene un manejo de informacion*/
+/*
+Caracteristicas P
+- Un weekset, add(), has(), remove(), dan un error si enviamos como parametroalgo que no sea un objeto
+- No tiene manera de hacer repeticiones o ciclos for in
+- Los weeksets no tienen keys(), values() por lo que no hay manera, via programacion de saber cuantos elementos hay dentro
+- No tienen for-each 
+- No tienen propiedad size
+*/
+
+/*Nota: Uso para manejar referencia hacia los objetos unicamente*/
