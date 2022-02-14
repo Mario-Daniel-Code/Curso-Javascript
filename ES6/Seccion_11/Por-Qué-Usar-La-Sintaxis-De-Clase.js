f0c7e01f250422447011fea896e48a85a0fe9c5b
Mario Martinez <mario.martinez1@gmail.com>
2022-02-14 17:21:17 -0600

    Por qué usar la sintaxis de clase

---
+++ ES6/Seccion_11/Por-Qué-Usar-La-Sintaxis-De-Clase.js
@@ -0,0 +1,12 @@
+/*-------------------Por qué usar la sintaxis de clase-----------------------*/
+
+/* 
+Caracteristicas de las clases
+- Las clases funcionan muy parecido a la declaracion LET
+- Todo el codigo dentro de la clase funciona en modo estricto "strict mode"
+- Todos los metodos no son enumerables
+- Todos los metodos internos no tienen un constructor, por consecuencia no se pueden llamar con el metodo new 
+- Llamar una clase sin el new dara un error
+- Intentar renombrar el nombre de la clase dentro de un metodo de la misma dara error
+- Las clases tienen metodos estaticos y privados 
+*/
